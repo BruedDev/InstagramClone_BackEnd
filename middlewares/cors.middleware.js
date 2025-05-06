a6e5939b707800dafc59c25a4a6cc694a632fb57
vanloc19 <tovanloc19@gmail.com>
2025-05-07 02:48:24 +0700

    update4

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -2,7 +2,19 @@ import cors from 'cors';
 
 const corsMiddleware = () => {
   const corsOptions = {
-    origin: true, // Cho phép tất cả origins
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://instagram-clone-seven-sable.vercel.app',
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
