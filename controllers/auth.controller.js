ba9252c8ba9ac3c5ddf852707170f584aeec0b56
vanloc19 <tovanloc19@gmail.com>
2025-05-08 10:57:30 +0700

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const login = async (req, res) => {
 
     // Set cookie
     res.cookie('token', token, cookieOptions);
-
+    res.header('Access-Control-Allow-Credentials', 'true');
     // Trả về token trong JSON response để frontend có thể sử dụng nếu cookie không hoạt động
     res.status(200).json({
       success: true,
